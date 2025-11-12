f18bbdd22bd20e1f1d65c1e5637df06857ddca00
Aamir Azad <82281117+aamirazad@users.noreply.github.com>
2025-11-12 14:41:44 -0500

    Start of chapter 4

---
+++ 4-understanding-ownership/src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    // Strings!
+    // string literals is what we've seen before
+    // like in here
+    println!("String literal!");
+    // The String type is different that it
+    // is not required to be immutable
+    let s = String::from("hello");
+    println!("{s}");
+    let mut message = String::from("hello");
+    message.push_str(", world!");
+    println!("{message}");
+    let s1 = String::from("hello");
+    // s2 takes the pointer and s1 is now invalid
+    let s2 = s1;
+    println!("{s2}");
+    // This gives the error
+    // borrow of moved value: `s1`
+    // println!("{s1}, world!");
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+    println!("s1 = {s1}, s2 = {s2}");
+    // We do not need to clone variables of known
+    // size (Copy trait) like numbers
+}
