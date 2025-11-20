b9bf966ec9659d45110c6d27b391e83d99aa9c7b
Aamir Azad <aamirmazad@gmail.com>
2025-11-20 10:31:36 -0500

    start slices

--- chapter-4-understanding-ownership/src/bin/references.rs
+++ chapter-4-understanding-ownership/src/bin/references.rs
@@ -1,5 +1,41 @@
 fn main() {
-	let s1 = String::from("hello");
-	let len calculate_length(&s1);
-	
+    let s1 = String::from("hello");
+    let len = calculate_length(&s1);
+    println!("The length of '{s1}' is {len}.");
+
+    let mut s = String::from("hello");
+    change(&mut s);
+    println!("{s}");
+    // Mutable references cannot give out another
+    // reference. For example this will fail
+    // let r1 = &mut s;
+    // let r2 = &mut s;
+    // println!("{r1}");
+    // But this works because r1 is not used
+    let _r1 = &mut s;
+    let r2 = &mut s;
+    println!("{r2}");
+    // We have to use curly braces to create a new
+    // scope in order to make another reference
+    let _a = no_dangle();
+}
+
+fn calculate_length(s: &String) -> usize {
+    // We borrow s here. But since we do not own
+    // it, we cannot modify it. For example
+    // s.push_str(", world"); would not work
+    s.len()
+}
+
+fn change(s: &mut String) {
+    s.push_str(", world");
+}
+
+fn no_dangle() -> String {
+    let s = String::from("hello");
+
+    s
+    // We cannot return a reference here because
+    // the s it is refering to would go out of scope
+    // before it is used creating a dangling pointer
 }
---
+++ chapter-4-understanding-ownership/src/bin/slices.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let mut s = String::from("hello world");
+    let word = first_word(&s);
+
+    s.clear();
+}
+// Here’s a small programming problem: write a function that takes a string of words separated by spaces and returns the first word it finds in that string. If the function doesn’t find a space in the string, the whole string must be one word, so the entire string should be returned.
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
