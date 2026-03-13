375368e17b6609ca33a0bac792414b8ce524d2e8
Aamir Azad <aamirmazad@gmail.com>
2026-03-12 21:19:09 -0400

    work on enums

--- chapter-6-enums-and-pattern-matching/src/bin/defining-enums.rs
+++ chapter-6-enums-and-pattern-matching/src/bin/defining-enums.rs
@@ -1,4 +1,12 @@
 #![allow(unused)]
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
 fn main() {
     #[derive(Debug)]
     enum IpAddr {
@@ -29,4 +37,36 @@ fn main() {
     let some_number = Some(5);
     let some_char = Some('e');
     let absent_number: Option<i32> = None;
+    let six = plus_one(some_number);
+    let none = plus_one(None);
+
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        _ => ,
+    }
 }
+
+fn value_in_sents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
+fn move_player(num_spaces: u8) {}
